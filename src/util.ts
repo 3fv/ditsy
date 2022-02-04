a30b5c744925b6a3c228557bc0a3cd2f47504572
Jonathan Glanz <jonathan.glanz@gmail.com>
2022-02-04 12:45:30 -0500

    wip

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,10 @@
 import { isDev } from "./dev"
-import { isFunction, isBoolean,isDefined, isString, isSymbol, isClass } from "@3fv/guard"
+import { isFunction, isBoolean, isDefined, isString, isSymbol, isClass } from "@3fv/guard"
 import { InjectableId } from "./injector"
 import { isEmpty, negate } from "lodash"
 import { Option } from "@3fv/prelude-ts"
 
-export function debug(test: boolean | any, ...args:any[]){
+export function debug(test: boolean | any, ...args: any[]) {
   if (isDev && !!test) {
     args = Option.of(test)
       .filter(negate(isBoolean))
@@ -15,49 +15,48 @@ export function debug(test: boolean | any, ...args:any[]){
   }
 }
 
-export function info(test: boolean | any, ...args:any[]) {
+export function info(test: boolean | any, ...args: any[]) {
   if (isDev && !!test) {
     args = Option.of(test)
       .filter(negate(isBoolean))
       .map(test => [test, ...args])
       .getOrElse(args)
 
+    if (typeof process !== "undefined" && process?.env?.["JEST_WORKER_ID"]) {
+      // IN JEST WORKER, DON'T WARN
+      return
+    }
     console.info(...args)
   }
 }
 
-export function warn(test: boolean | any, ...args:any[]) {
+export function warn(test: boolean | any, ...args: any[]) {
   if (isDev && !!test) {
     args = Option.of(test)
       .filter(negate(isBoolean))
       .map(test => [test, ...args])
       .getOrElse(args)
 
+    if (typeof process !== "undefined" && process?.env?.["JEST_WORKER_ID"]) {
+      // IN JEST WORKER, DON'T WARN
+      return
+    }
     console.warn(...args)
   }
 }
 
-
-
 // Help user locate misapplied decorators.
 export function targetHint<T>(target: T) {
-
-  return isFunction(target) ?
-    target.name ?? target.constructor?.name :
-    target?.toString()
+  return isFunction(target) ? target.name ?? target.constructor?.name : target?.toString()
 }
 
-
-export function isId(id: any): id is InjectableId<any>{
+export function isId(id: any): id is InjectableId<any> {
   return isString(id) || isSymbol(id) || isClass(id)
 }
 
 export function idHint(id: InjectableId<any>) {
-  return isFunction(id) ?
-    id.name ?? id.toString() :
-    id?.toString()
+  return isFunction(id) ? id.name ?? id.toString() : id?.toString()
 }
-
 
 export const isNotDefined = negate(isDefined)
 
