437c0a64bbb32e5fded9a7b05986b17f64be9369
Jonathan Glanz <jonathan.glanz@gmail.com>
2024-08-12 21:03:48 -0400

    wip

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,18 +3,18 @@ export default {
   rootDir: process.cwd(),
   verbose: true,
   testMatch: [
-    "<rootDir>/src/**/?(*)(test|spec).(ts|js|tsx)"
+    "<rootDir>/src/__tests__/*.spec.(ts|js|tsx)"
   ],
   modulePathIgnorePatterns: [
     ".*\\.layers.*",
     "<rootDir>\\/lib\\/.*"
-  
+
   ],
   moduleDirectories: ["node_modules"],
   moduleFileExtensions: ["ts", "tsx", "js"],
   coverageReporters: ["html"],
   transform: {
-    "^.*src/.+\\.tsx?$": "ts-jest"
+    "\\.tsx?$": "ts-jest"
     // SWC decorator metadata missing, needs debugging - another time
     //"\\.tsx?$": ["@swc/jest"]
   }
